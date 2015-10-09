9c4ae869e57dfac97bf1fb3b2bccb4804ffe8677
mazong1123 <mazong1123@gmail.com>
2015-10-09 23:44:13 +0800

    Added dist version.

---
+++ dist/scripts/react-dom.js
@@ -0,0 +1,10 @@
+/*!
+ * =============================================================
+ * Chitu Web App v1.0.0 (https://github.com/mazong1123/ratchet-pro)
+ * Copyright 2015 mazong1123
+ * Licensed under MIT (https://github.com/mazong1123/chitu/blob/master/LICENSE)
+ *
+ * v1.0.0 designed by @mazong1123.
+ * =============================================================
+ */
+!function(a){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=a(require("react"));else if("function"==typeof define&&define.amd)define(["react"],a);else{var b;b="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:this,b.ReactDOM=a(b.React)}}(function(a){return a.__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED});
