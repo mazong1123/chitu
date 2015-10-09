3d84c7a7cce1801dff69dc37b34b8bd17b87f051
mazong1123 <mazong1123@gmail.com>
2015-10-09 11:06:30 +0800

    Setup the project structure. Setup grunt tasks.

---
+++ dist/scripts/app-index.js
@@ -0,0 +1,10 @@
+/*!
+ * =============================================================
+ * Chitu Web App v1.0.0 (https://github.com/mazong1123/ratchet-pro)
+ * Copyright 2015 mazong1123
+ * Licensed under MIT (https://github.com/mazong1123/chitu/blob/master/LICENSE)
+ *
+ * v1.0.0 designed by @mazong1123.
+ * =============================================================
+ */
+!function(){var a=new window.RATCHET.Class.PageManager;a.ready(function(){})}();
